086e12a0a6dcd2a1072e3b0ff0c86cf40d2cb99b
Christoph Wirsching <Christoph@Christophs-MBP.fritz.box>
2017-10-08 20:48:28 +0200

    Added Field Check Submit with Keyboard

--- fuse/pages/Register.js
+++ fuse/pages/Register.js
@@ -15,7 +15,6 @@ var areCredentialsValid = RegisterObservable(function() {
 function register() {
 	/*
 		To Do:
-		- Alle Felder checken wenn mit Tastatur abgeschickt wird
 		- Email Adresse prüfen / Email Adress Prüfung in Funktion auslagern
 		- Passwort und Repeatpasswort prüfen
 		- Passwort Komplexität einbauen
@@ -26,58 +25,71 @@ function register() {
 	Observable.ShowOverlay.value = true; // Overlay einblenden
 	Observable.ShowLoadingIndicator.value = true; // Loading Symbol einblenden
 
-    var requestObject = {
-    	first_name: Firstname.value, 
-    	last_name: Lastname.value, 
-    	password: Password.value, 
-    	email: Email.value
-    };
+	if(Firstname.value != "" && Lastname.value != "" && Email.value != "" && Password.value != "" && RepeatPassword.value != ""){
 
-	fetch('http://app.scotty2nd.square7.ch/api/customer/add', {
-	  	method: 'POST',
-	  	headers: { "Content-type": "application/json", "Accept": "application/json" },
-	  	body: JSON.stringify(requestObject)
-  	}).then(function(response) {
-	  	return response.json();    // This returns a promise
-  	}).then(function(data) {
-	    // Server Antwort verarbeiten
-	    if(data.error == false){
-		    console.log('do something');
-		    console.log(data.error);
-		    console.log(data.message);
+	    var requestObject = {
+	    	first_name: Firstname.value, 
+	    	last_name: Lastname.value, 
+	    	password: Password.value, 
+	    	email: Email.value
+	    };
 
-    		Observable.ShowLoadingIndicator.value = false // Loading Symbol ausblenden
-			
-			Observable.Modal.Background = Observable.Colors.Success; // Modal Hintergrundfarbe setzen 
-			Observable.Modal.Headline = ""; // Modal Dachzeile setzen
-			Observable.Modal.Title = "Glückwunsch"; // Modal Titel setzen
-			Observable.Modal.Message.value = data.message; // Modal Text setzen
-			Observable.Modal.Visibility.value = true; // Modal sichtbar machen
+		fetch('http://app.scotty2nd.square7.ch/api/customer/add', {
+		  	method: 'POST',
+		  	headers: { "Content-type": "application/json", "Accept": "application/json" },
+		  	body: JSON.stringify(requestObject)
+	  	}).then(function(response) {
+		  	return response.json();    // This returns a promise
+	  	}).then(function(data) {
+		    // Server Antwort verarbeiten
+		    if(data.error == false){
+			    console.log('do something');
+			    console.log(data.error);
+			    console.log(data.message);
 
-		    Firstname.value = '';		//Set Field to blank
-		    Lastname.value = '';
-		    Email.value = '';
-		    Password.value = '';
-		    RepeatPassword.value = '';
-		}else if(data.error == true){
-	    	Observable.ShowLoadingIndicator.value = false // Loading Symbol ausblenden
+	    		Observable.ShowLoadingIndicator.value = false // Loading Symbol ausblenden
+				
+				Observable.Modal.Background = Observable.Colors.Success; // Modal Hintergrundfarbe setzen 
+				Observable.Modal.Headline = ""; // Modal Dachzeile setzen
+				Observable.Modal.Title = "Glückwunsch"; // Modal Titel setzen
+				Observable.Modal.Message.value = data.message; // Modal Text setzen
+				Observable.Modal.Visibility.value = true; // Modal sichtbar machen
+
+			    Firstname.value = '';		//Set Field to blank
+			    Lastname.value = '';
+			    Email.value = '';
+			    Password.value = '';
+			    RepeatPassword.value = '';
+			}else if(data.error == true){
+		    	Observable.ShowLoadingIndicator.value = false // Loading Symbol ausblenden
+
+				Observable.Modal.Background = Observable.Colors.Error; // Modal Hintergrundfarbe setzen 
+				Observable.Modal.Headline = "Oops!"; // Modal Dachzeile setzen
+				Observable.Modal.Title = "Es ist ein Fehler aufgetreten."; // Modal Titel setzen
+				Observable.Modal.Message.value = data.message; // Modal Text setzen
+				Observable.Modal.Visibility.value = true; // Modal sichtbar machen
+		    }
+		}).catch(function(error) {
+		    // An error occurred somewhere in the Promise chain
+		    Observable.ShowLoadingIndicator.value = false; // Loading Symbol ausblenden
 
 			Observable.Modal.Background = Observable.Colors.Error; // Modal Hintergrundfarbe setzen 
 			Observable.Modal.Headline = "Oops!"; // Modal Dachzeile setzen
 			Observable.Modal.Title = "Es ist ein Fehler aufgetreten."; // Modal Titel setzen
-			Observable.Modal.Message.value = data.message; // Modal Text setzen
+			Observable.Modal.Message.value = "Ein unbekannter Fehler ist aufgetreten."; // Modal Text setzen
 			Observable.Modal.Visibility.value = true; // Modal sichtbar machen
-	    }
-	}).catch(function(error) {
-	    // An error occurred somewhere in the Promise chain
-	    Observable.ShowLoadingIndicator.value = false; // Loading Symbol ausblenden
+		});
+	}else{
+		// Email Adresse ist ungültig
+		// Nicht alle Felder ausgefüllt
+		Observable.ShowLoadingIndicator.value = false; // Loading Symbol ausblenden
 
 		Observable.Modal.Background = Observable.Colors.Error; // Modal Hintergrundfarbe setzen 
 		Observable.Modal.Headline = "Oops!"; // Modal Dachzeile setzen
 		Observable.Modal.Title = "Es ist ein Fehler aufgetreten."; // Modal Titel setzen
-		Observable.Modal.Message.value = "Ein unbekannter Fehler ist aufgetreten."; // Modal Text setzen
+		Observable.Modal.Message.value = "Bitte füllen Sie alle Felder aus."; // Modal Text setzen
 		Observable.Modal.Visibility.value = true; // Modal sichtbar machen
-	});
+	}
 }
 
 function goBack() {
