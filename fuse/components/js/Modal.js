05092f00683355bef840c3f8efbbe7e23424ca6b
Christoph Wirsching <Christoph@Christophs-MBP.fritz.box>
2017-12-20 22:21:35 +0100

    New Modal Aproach

--- fuse/components/js/Modal.js
+++ fuse/components/js/Modal.js
@@ -1,9 +1,5 @@
 var include = require("/Main");
 
-this.BackgroundColor.value = include.modal.color;
-this.Headline.value = include.modal.headline;
-this.Title.value = include.modal.title;
-
 function OkClicked() {
 	include.modal.visibility.value = false; 	// Fehlermeldung ausblenden
 	include.showOverlay.value = false; 			// Overlay ausblenden
